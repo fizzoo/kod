c57f0667b2051792fd6188b21f5827306edb4c5b
fizzoo <vixirfizz@gmail.com>
2016-10-11 15:18:03 +0200

    Add calculate_cost

--- qanat/q.hs
+++ qanat/q.hs
@@ -1,20 +1,27 @@
-import Data.List.Split (splitOn)
-
 dig :: Floating a => a -> a -> a 
 dig from to = 0.5 * to * to - 0.5 * from * from
   
+calculate_costs :: Floating a => a -> a -> [a] -> a
+calculate_costs w h ls = lefts ls + rights ls + begin + end
+  where
+  heightmult = w / h
+  begin = dig 0 (head ls * (1.0 + heightmult)/ 2)
+  end = dig 0 (((w - last ls)/2) + h)
+  lefts (a:b:rest) = dig 0 ((b-a + heightmult*(b-a))/2) + lefts (b:rest)
+  lefts _ = 0                   -- 1 or 0 elements
+  rights (a:b:rest) = 0.0
+  rights _ = 0                  -- 1 or 0 elements
+
 qanat :: Floating a => a -> a -> a -> [a]
 qanat w h n = cost:placements
   where
-    cost = dig 0 2.25 + dig 0 2.25 + dig 1.5 5.25 + dig 0 5.25
-    g = w/3.0
+    cost = calculate_costs w h placements
     placements = [3.0]
   
 main :: IO ()
 main = do
   line <- getLine
-  let intstr = splitOn " " line
-      ints   = map read intstr
+  let ints   = map read $ words line
       answers = case ints of [a, b, c] -> qanat a b c
                              _ -> error "Invalid parameters"
   mapM_ print answers
