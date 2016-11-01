bcfd86e7e56a5f7dbb5e37c8f55c4e25a56ee9b8
fizzoo <vixirfizz@gmail.com>
2016-11-01 13:32:06 +0100

    Qanat that works aside from convergence speed

--- qanat/q.hs
+++ qanat/q.hs
@@ -1,8 +1,8 @@
 dig :: Floating a => a -> a
 dig x = 0.5 * x * x
-  
-calculate_costs :: Floating a => a -> [a] -> a
-calculate_costs h ls = rec ls
+
+calculateCosts :: Double -> [Double] -> Double
+calculateCosts h = rec
   where
   rec (a:b:rest) = left + right + rec (b:rest)
     where
@@ -12,12 +12,35 @@ calculate_costs h ls = rec ls
   rec [x] = dig (h*x)
   rec _ = error "[] rec call"
 
-qanat :: Floating a => a -> a -> a -> [a]
-qanat w h n = cost:placements
+placeBetween :: Floating a => a -> a -> a -> a
+placeBetween a b h = -(a+b)*(h**2-1)/2
+
+uniform :: Integer -> Double -> [Double]
+uniform n max = map ((*(max/(fromInteger (n-1)))) . fromInteger) [0..(n-1)]
+
+improveGuess :: Double -> [Double] -> Double -> [Double]
+improveGuess prev (_:c:r) h = prev : improveGuess (placeBetween prev c h) (c:r) h
+improveGuess prev [x] _ = prev:[x]
+improveGuess _ _ _ = error "[] improveguess call"
+
+lowdiff :: [Double] -> [Double] -> Bool
+lowdiff a b = maximum (map abs (zipWith (-) a b)) < 0.00001
+
+keepImproving :: [Double] -> Double -> [Double]
+keepImproving prev h
+  | lowdiff prev cur = cur
+  | otherwise = keepImproving cur h
   where
-    cost = w * w * calculate_costs (h/w) placements
-    placements = 0 : (map (/w) [48.0, 108.0]) ++ [1]
-  
+    cur = improveGuess (head prev) (tail prev) h
+
+qanat :: Integer -> Integer -> Integer -> [Double]
+qanat w h n = cost : map (*wf) (tail $ init placements)
+  where
+  cost = wf * wf * calculateCosts heightmult placements
+  placements = keepImproving (uniform (n+2) 1.0) heightmult :: [Double]
+  wf = fromIntegral w :: Double
+  heightmult = fromIntegral h / wf :: Double
+
 main :: IO ()
 main = do
   line <- getLine
