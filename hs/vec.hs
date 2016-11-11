2a17d34bf1581a21c486049e24e2fb1373dd7416
fizzoo <vixirfizz@gmail.com>
2016-11-11 15:33:01 +0100

    Slight expansion on vec

--- hs/vec.hs
+++ hs/vec.hs
@@ -1,11 +1,26 @@
-import qualified Data.Vector.Unboxed as V
-import           System.Random       (randomIO)
+import           Data.Vector   ((!))
+import qualified Data.Vector   as V
+import           System.Random (randomIO)
 
 
 main :: IO ()
 main = do
-    vector <- V.generateM (16*16) gen
-    print vector
-      where
-        gen :: a -> IO Bool
-        gen = const randomIO
+  genrng
+  print l10
+  print $ dim10 ! 1 ! 1
+
+genrng :: IO ()
+genrng = do
+  vector <- V.generateM (16*16) gen
+  print vector
+    where
+      gen :: a -> IO Bool
+      gen = const randomIO
+
+
+l10 :: V.Vector Int
+l10 = V.fromList [1..10]
+
+dim10 :: V.Vector (V.Vector Int)
+dim10 = V.replicate 10 $ V.fromList [1..10]
+
